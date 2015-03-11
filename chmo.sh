974f097f4b2a92bd6a7e177b39cb421fb9d7cf14
Ezra Radoff <ezra@ssytems.info>
2015-03-11 15:23:31 +0200

    chmo version 1

---
+++ chmo.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mkdir -p $3
+super=$(echo $3 | cut -f 1 -d "/")
+chown -R $1:$1 $super
+chmod -R $2 $super
+echo "usage chmo <user> <perms> <path>"
+echo "example: chmo ezra 664 /home/ezra"
+
